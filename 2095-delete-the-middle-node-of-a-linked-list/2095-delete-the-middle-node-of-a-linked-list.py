3a4204af1e611b67d1f6106e774214796204f877
Sushilkumar <sushilmevada6@gmail.com>
2026-06-22 11:46:49 +0530

    Time: 84 ms (69.52%), Space: 62.2 MB (75.91%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
@@ -5,20 +5,17 @@
 #         self.next = next
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return None
+        
         start= head
         half_p = head
         half_prev = None
-        does_move = False
-        while head != None:
-            if does_move: 
-                half_prev = half_p
-                half_p = half_p.next
-                does_move=False
-            else:
-                does_move=True
-            head = head.next
-        if half_p!=None:
-            if half_prev != None:
-                half_prev.next = half_p.next
-            else:start = half_prev
+        
+        while head and head.next:
+            half_prev = half_p
+            half_p = half_p.next
+            head = head.next.next
+
+        half_prev.next = half_p.next
         return start
