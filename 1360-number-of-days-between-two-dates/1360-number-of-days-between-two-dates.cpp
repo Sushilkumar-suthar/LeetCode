f2947961bbd64fcacf5df7eaf021320cecb81f61
Sushilkumar <sushilmevada6@gmail.com>
2026-07-14 16:49:24 +0530

    Time: 0 ms (100%), Space: 8 MB (60.88%) - LeetHub

---
+++ 1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isLeap(int year) {
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
+
+    int daysFrom1971(string date) {
+        int year = stoi(date.substr(0, 4));
+        int month = stoi(date.substr(5, 2));
+        int day = stoi(date.substr(8, 2));
+
+        static vector<int> daysInMonth = {
+            31,28,31,30,31,30,31,31,30,31,30,31
+        };
+
+        int days = 0;
+
+        for (int y = 1971; y < year; y++)
+            days += isLeap(y) ? 366 : 365;
+
+        for (int m = 1; m < month; m++) {
+            days += daysInMonth[m - 1];
+            if (m == 2 && isLeap(year))
+                days++;
+        }
+
+        days += day;
+
+        return days;
+    }
+
+    int daysBetweenDates(string date1, string date2) {
+        return abs(daysFrom1971(date1) - daysFrom1971(date2));
+    }
+};
