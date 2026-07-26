abe2cb4d88053316b092a2a36d58fd6e069cab5b
Sushilkumar <sushilmevada6@gmail.com>
2026-07-26 22:42:32 +0530

    Time: 0 ms (100%), Space: 8.7 MB (14.9%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        } 
+        
+        double guess = x;
+
+        while (fabs(guess * guess - x) > 1e-6) {
+            guess = (guess + x / guess) / 2;
+        }
+        return std::floor(guess);
+    }
+};
