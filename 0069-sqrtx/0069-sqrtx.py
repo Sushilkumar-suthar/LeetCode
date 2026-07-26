eab1e4b6c2a85a64686b3264b4b5182c7ed80652
Sushilkumar <sushilmevada6@gmail.com>
2026-07-26 22:38:05 +0530

    Time: 5 ms (35.19%), Space: 19.5 MB (6.28%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,9 @@
+import math
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:return 0
+        guess = x
+        while abs(guess * guess - x) > 0.000001:
+            guess = (guess + x / guess) / 2
+
+        return math.floor(guess)
