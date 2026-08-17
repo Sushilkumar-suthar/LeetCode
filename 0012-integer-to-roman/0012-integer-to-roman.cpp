12cc88cc752aeb0a0341334785f3b4ba7d80ce89
Sushilkumar <sushilmevada6@gmail.com>
2026-08-17 22:31:37 +0530

    Time: 0 ms (100%), Space: 9.3 MB (83.43%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<pair<int, string>> roman = {
-            {1000, "M"},
-            {900,  "CM"},
-            {500,  "D"},
-            {400,  "CD"},
-            {100,  "C"},
-            {90,   "XC"},
-            {50,   "L"},
-            {40,   "XL"},
-            {10,   "X"},
-            {9,    "IX"},
-            {5,    "V"},
-            {4,    "IV"},
-            {1,    "I"}
+        static const string thousands[] = {"", "M", "MM", "MMM"};
+        static const string hundreds[] = {
+            "", "C", "CC", "CCC", "CD", "D",
+            "DC", "DCC", "DCCC", "CM"
+        };
+        static const string tens[] = {
+            "", "X", "XX", "XXX", "XL", "L",
+            "LX", "LXX", "LXXX", "XC"
+        };
+        static const string ones[] = {
+            "", "I", "II", "III", "IV", "V",
+            "VI", "VII", "VIII", "IX"
         };
 
-        string result;
-
-        for (auto &[value, symbol] : roman) {
-            while (num >= value) {
-                result += symbol;
-                num -= value;
-            }
-        }
-
-        return result;
+        return thousands[num / 1000] +
+               hundreds[(num % 1000) / 100] +
+               tens[(num % 100) / 10] +
+               ones[num % 10];
     }
 };
