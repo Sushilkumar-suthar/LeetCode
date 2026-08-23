af4bd050d5e5f4e7e3c79acbf6bc3bac1f48f6b2
Sushilkumar <sushilmevada6@gmail.com>
2026-08-23 22:20:05 +0530

    Time: 241 ms (5.18%), Space: 19.6 MB (9.31%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
@@ -1,15 +1,26 @@
 class Solution:
-    def sortByBits(self, arr: List[int]) -> List[int]:
-        buckets = [[] for _ in range(14)]
+    def sortByBits(self, arr):
+        count = [0] * 15
 
         for x in arr:
-            bits = x.bit_count()
-            buckets[bits].append(x)
+            count[x.bit_count()] += 1
+
+        buckets = [[] for _ in range(15)]
+
+        for x in arr:
+            buckets[x.bit_count()].append(x)
 
         ans = []
 
-        for bucket in buckets:
-            bucket.sort()
-            ans.extend(bucket)
+        for b in range(15):
+            bucket = buckets[b]
+
+            freq = [0] * 10001
+            for x in bucket:
+                freq[x] += 1
+
+            for x in range(10001):
+                if freq[x]:
+                    ans.extend([x] * freq[x])
 
         return ans
